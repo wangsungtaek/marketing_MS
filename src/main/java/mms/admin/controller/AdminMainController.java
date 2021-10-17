4341adbaf825d6d9e2ed6426135a8798f4e744c0
wangsungtaek <tjdxorv@naver.com>
2021-10-17 23:45:33 +0900

    add project

---
+++ src/main/java/mms/admin/controller/AdminMainController.java
@@ -0,0 +1,15 @@
+package mms.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("mms")
+public class AdminMainController {
+
+    @GetMapping("")
+    public String dashboardPage() {
+        return "index";
+    }
+}
